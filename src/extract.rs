a65af041e508b41fa6a207b929d68af6d402a496
Remy Wang <remy.sucre@gmail.com>
2019-10-19 15:58:28 -0700

    Reorg

---
+++ src/extract.rs
@@ -0,0 +1,227 @@
+use crate::{EGraph, Math, Schema, Sparsity};
+
+use egg::{
+    define_term,
+    egraph::{AddResult, EClass},
+    expr::{Expr, Language, QuestionMarkName, Id, RecExpr},
+    //extract::{calculate_cost, Extractor},
+    parse::ParsableLanguage,
+    pattern::{Applier, Rewrite, WildMap},
+};
+use lp_modeler::solvers::{GurobiSolver, SolverTrait};
+use lp_modeler::dsl::*;
+use lp_modeler::format::lp_format::LpFileFormat;
+use bimap::BiMap;
+use std::collections::{HashMap, HashSet};
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use ordered_float::NotNan;
+
+pub fn extract(egraph: EGraph, root: Id) {
+    let mut problem = LpProblem::new("wormhole", LpObjective::Minimize);
+
+    let br = "bq".to_owned() + &root.to_string();
+
+    let mut var_bns = BiMap::<&Expr<Math, Id>, SymVar>::new();
+    let mut var_bqs = BiMap::<Id, SymVar>::new();
+
+    let mut cs = HashSet::new();
+
+    for c in egraph.classes() {
+        cs.insert(c.id);
+        for e in c.nodes.iter() {
+            for class in e.children.iter() {
+                cs.insert(*class);
+            }
+        }
+    }
+
+    let mut count = 0;
+
+    for _ in egraph.classes() {
+        count += 1;
+    }
+
+    let clen = cs.len();
+
+    assert_eq!(count, clen, "DIFFERS BY {}", clen - count);
+
+    for c in egraph.classes() {
+        let bqv = "bq".to_owned() + &c.id.to_string();
+        let bq = LpBinary::new(&bqv);
+        var_bqs.insert(c.id, SymVar(bq));
+
+        for e in c.nodes.iter() {
+            let mut s = DefaultHasher::new();
+            e.hash(&mut s);
+            let bnv = "bn".to_owned() + &s.finish().to_string();
+            let bn = LpBinary::new(&bnv);
+            var_bns.insert_no_overwrite(e, SymVar(bn));
+        }
+    };
+
+    let obj_vec: Vec<LpExpression> = {
+        var_bns.iter().map(|(e, bin)| {
+            let coef = cost(&egraph, e);
+            coef as f32 * &bin.0
+        }).collect()
+    };
+
+    println!("after cost");
+    println!("{:?}", obj_vec.len());
+
+    problem += binary_sum(&obj_vec); //obj_vec.sum();// sum_lp(obj_vec);
+
+    println!("after cost");
+
+    // Br: must pick root
+    problem += (0 + &var_bqs.get_by_left(&root).unwrap().0).equal(1);
+
+    println!("before gq");
+
+    for class in egraph.classes() {
+        // Gq: Bq => OR Bn in q.nodes
+        // (not Bq) or (OR Bn)
+        // (1-Bq) + (sum Bn) > 0
+        problem += ((1-&var_bqs.get_by_left(&class.id).unwrap().0) + sum(&class.nodes, |n| &var_bns.get_by_left(&n).unwrap().0)).ge(1);
+
+        for node in class.iter() {
+            // Fn: Bn => AND Bq in n.children
+            // (not Bn) or (AND Bq)
+            let bn = &var_bns.get_by_left(&node).unwrap().0;
+            for class in node.children.iter() {
+                // (1-Bn) + bq > 0
+                let bq = &var_bqs.get_by_left(&class).unwrap().0;
+                problem += ((1-bn) + bq).ge(1);
+            }
+        }
+    }
+
+    println!("after gq");
+
+    let solver = GurobiSolver::new();
+    let result = solver.run(&problem);
+
+    println!("{:?}", result);
+    assert!(result.is_ok(), result.unwrap_err());
+
+    let (_solver_status, var_values) = result.unwrap();
+
+    let mut selected = HashSet::new();
+
+    for (var_name, var_value) in &var_values {
+        let int_var_value = *var_value as u32;
+        if int_var_value == 1{
+            if let Some(&v) = var_bns.get_by_right(&SymVar(LpBinary::new(var_name))) {
+                //println!("{}", v.op);
+                selected.insert(v);
+            }
+        }
+    }
+
+    let best = best_expr(&egraph, root, &selected);
+    println!("{}", best.pretty(40));
+}
+
+
+fn best_expr(egraph: &EGraph, class: Id, selected: &HashSet<&Expr<Math, Id>>) -> RecExpr<Math> {
+    let eclass = egraph.find(class);
+    let best_node = egraph[eclass]
+        .iter()
+        .find(|n| selected.contains(n))
+        .expect("no node selected in class");
+
+    best_node
+        .clone()
+        .map_children(|child| best_expr(egraph, child, selected))
+        .into()
+}
+
+#[derive(PartialEq)]
+struct SymVar(LpBinary);
+
+impl Eq for SymVar {}
+
+impl Hash for SymVar {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.name.hash(state);
+    }
+}
+
+fn cost(egraph: &EGraph, expr: &Expr<Math, Id>) -> usize {
+    match expr.op {
+        Math::Add => {
+            assert_eq!(expr.children.len(), 2);
+            let x = expr.children[0];
+            let y = expr.children[1];
+
+            egraph[x].metadata.nnz + egraph[y].metadata.nnz
+        },
+        Math::Mul => {
+            assert_eq!(expr.children.len(), 2, "wrong length in mul");
+            let x_id = &expr.children[0];
+            let x = &egraph[*x_id].metadata;
+            let y_id = &expr.children[1];
+            let y = &egraph[*y_id].metadata;
+
+            let schema = x. schema.union(&y.schema);
+
+            let sparsity = x.sparsity.merge(&y.sparsity, Math::Mul);
+
+            let nnz = if let Schema::Schema(s1) = &schema {
+                let vol: usize = s1.values().product();
+                match sparsity {
+                    Sparsity::Sparse(s2) => {
+                        NotNan::from(vol as f64) * (NotNan::from(1 as f64)-s2)
+                    },
+                    _ => NotNan::from(0 as f64)
+                }
+            } else {
+                panic!("wrong schema in mul")
+            };
+            nnz.round() as usize
+        },
+        Math::Agg => {
+            assert_eq!(expr.children.len(), 2, "wrong length in mul");
+            let i_id = &expr.children[0];
+            let i = &egraph[*i_id].metadata;
+            let body_id = &expr.children[1];
+            let body = &egraph[*body_id].metadata;
+
+            if let Schema::Dims(_, size) = i.schema {
+                match body.sparsity {
+                    Sparsity::Sparse(s) => (NotNan::from(size as f64) * s).round() as usize,
+                    _ => size
+                }
+            } else {
+                panic!("wrong schema in dimension")
+            }
+
+        },
+        _ => 0
+    }
+}
+
+pub fn sum_lp<T>(expr: Vec<T>) -> LpExpression where T: Into<LpExpression> + Clone {
+    if let Some(first) = expr.first() {
+        let mut res = first.clone().into();
+        for e in expr.iter() {
+            let prev = &res;
+            res = LpExpression::AddExpr(Box::new(e.clone().into()), Box::new(prev.clone().into()));
+        }
+        res
+    } else {
+        panic!("vector should have at least one element");
+    }
+}
+
+fn binary_sum(expr: &[LpExpression]) -> LpExpression
+{
+    let l = expr.len();
+    assert_ne!(l, 0);
+    match l {
+        1 => expr[0].clone(),
+        _ => LpExpression::AddExpr(Box::new(binary_sum(&expr[(l/2)..])),
+                                   Box::new(binary_sum(&expr[0..(l/2)])))
+    }
+}
---
+++ src/rules.rs
@@ -0,0 +1,293 @@
+use crate::{Math, Meta, EGraph, Schema};
+
+use egg::{
+    egraph::{AddResult},
+    //expr::{Expr, Language, QuestionMarkName, Id, RecExpr},
+    expr::{Expr, QuestionMarkName},
+    //extract::{calculate_cost, Extractor},
+    parse::ParsableLanguage,
+    pattern::{Applier, Rewrite, WildMap},
+};
+
+use smallvec::smallvec;
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+
+#[rustfmt::skip]
+pub fn rules() -> Vec<Rewrite<Math, Meta>> {
+    let rw = |name, l, r| Math::parse_rewrite::<Meta>(name, l, r).unwrap();
+    vec![
+        rw("+-commutative", "(+ ?a ?b)", "(+ ?b ?a)"),
+        rw("*-commutative", "(* ?a ?b)", "(* ?b ?a)"),
+
+        rw("associate-+r+", "(+ ?a (+ ?b ?c))", "(+ (+ ?a ?b) ?c)"),
+        rw("associate-+l+", "(+ (+ ?a ?b) ?c)", "(+ ?a (+ ?b ?c))"),
+        rw("associate-+r-", "(+ ?a (- ?b ?c))", "(- (+ ?a ?b) ?c)"),
+        rw("associate-+l-", "(+ (- ?a ?b) ?c)", "(- ?a (- ?b ?c))"),
+        rw("associate--r+", "(- ?a (+ ?b ?c))", "(- (- ?a ?b) ?c)"),
+        rw("associate--l+", "(- (+ ?a ?b) ?c)", "(+ ?a (- ?b ?c))"),
+        rw("associate--l-", "(- (- ?a ?b) ?c)", "(- ?a (+ ?b ?c))"),
+        rw("associate--r-", "(- ?a (- ?b ?c))", "(+ (- ?a ?b) ?c)"),
+        rw("associate-*r*", "(* ?a (* ?b ?c))", "(* (* ?a ?b) ?c)"),
+        rw("associate-*l*", "(* (* ?a ?b) ?c)", "(* ?a (* ?b ?c))"),
+        rw("associate-*r/", "(* ?a (/ ?b ?c))", "(/ (* ?a ?b) ?c)"),
+        rw("associate-*l/", "(* (/ ?a ?b) ?c)", "(/ (* ?a ?c) ?b)"),
+        rw("associate-/r*", "(/ ?a (* ?b ?c))", "(/ (/ ?a ?b) ?c)"),
+        rw("associate-/l*", "(/ (* ?b ?c) ?a)", "(/ ?b (/ ?a ?c))"),
+        rw("associate-/r/", "(/ ?a (/ ?b ?c))", "(* (/ ?a ?b) ?c)"),
+        rw("associate-/l/", "(/ (/ ?b ?c) ?a)", "(/ ?b (* ?a ?c))"),
+
+        rw("subst-+",      "(subst ?e ?v (+ ?a ?b))",     "(+ (subst ?e ?v ?a) (subst ?e ?v ?b))"),
+        rw("subst-*",      "(subst ?e ?v (* ?a ?b))",     "(* (subst ?e ?v ?a) (subst ?e ?v ?b))"),
+        rw("subst-matrix", "(subst ?e ?v (mat ?a ?i ?j ?z))", "(mat ?a (subst ?e ?v ?i) (subst ?e ?v ?j) ?z)"),
+        rw("subst-lit",    "(subst ?e ?v (lit ?n))",      "(lit ?n)"),
+
+        rw("matrix-swap-dims", "(mat ?x ?i ?j ?z)", "(mat ?x ?j ?i ?z)"),
+
+        rw("distribute-lft-in",    "(* ?a (+ ?b ?c))",        "(+ (* ?a ?b) (* ?a ?c))"),
+        rw("distribute-rgt-in",    "(* ?a (+ ?b ?c))",        "(+ (* ?b ?a) (* ?c ?a))"),
+        rw("distribute-lft-out",   "(+ (* ?a ?b) (* ?a ?c))", "(* ?a (+ ?b ?c))"),
+        rw("distribute-lft-out--", "(- (* ?a ?b) (* ?a ?c))", "(* ?a (- ?b ?c))"),
+        rw("distribute-rgt-out",   "(+ (* ?b ?a) (* ?c ?a))", "(* ?a (+ ?b ?c))"),
+        rw("distribute-rgt-out--", "(- (* ?b ?a) (* ?c ?a))", "(* ?a (- ?b ?c))"),
+        rw("distribute-lft1-in",   "(+ (* ?b ?a) ?a)",        "(* (+ ?b 1) ?a)"),
+        rw("distribute-rgt1-in",   "(+ ?a (* ?c ?a))",        "(* (+ ?c 1) ?a)"),
+
+        rw("pullup-add",    "(sum ?i (+ ?a ?b))",            "(+ (sum ?i ?a) (sum ?i ?b))"),
+        rw("pushdown-add",  "(+ (sum ?i ?a) (sum ?i ?b))",  "(sum ?i (+ ?a ?b))",),
+
+        rw("swap-agg", "(sum ?i (sum ?j ?a))", "(sum ?j (sum ?i ?a))"),
+
+        //Rewrite::new(
+        //    "foundit",
+        //    Math::parse_pattern(
+        //        "(+ (sum ?i (sum ?j (+ (* (mat ?x ?i ?j ?z) (mat ?x ?i ?j ?z)) (+ \
+        //         (* (mat ?x ?i ?j ?z) (sum ?k (* (mat ?u ?i ?k ?z) (mat ?v ?k ?j ?z)))) \
+        //         (* (mat ?x ?i ?j ?z) (sum ?k (* (mat ?u ?i ?k ?z) (mat ?v ?k ?j ?z)))))))) \
+        //         (sum ?c (sum ?a (* \
+        //         (sum ?b (* (mat ?u ?a ?b ?z) (mat ?u ?b ?c ?z)))\
+        //         (sum ?d (* (mat ?v ?a ?d ?z) (mat ?v ?d ?c ?z)))))))",).unwrap(),
+        //    Foundit,
+        //),
+
+        Rewrite::new(
+            "agg-subst",
+            Math::parse_pattern("(subst ?e ?v1 (sum ?v2 ?body))",).unwrap(),
+            SubstAgg {
+                e: "?e".parse().unwrap(),
+                v1: "?v1".parse().unwrap(),
+                v2: "?v2".parse().unwrap(),
+                body: "?body".parse().unwrap(),
+            },
+        ),
+        Rewrite::new(
+            "sum_i_a",
+            Math::parse_pattern("(sum ?i ?a)").unwrap(),
+            SumIA {
+                i: "?i".parse().unwrap(),
+                a: "?a".parse().unwrap(),
+            },
+        ),
+        Rewrite::new(
+            "pullup_mul",
+            Math::parse_pattern("(sum ?i (* ?a ?b))").unwrap(),
+            PullMul {
+                i: "?i".parse().unwrap(),
+                a: "?a".parse().unwrap(),
+                b: "?b".parse().unwrap(),
+            }
+        ),
+        Rewrite::new(
+            "pushdown_mul",
+            Math::parse_pattern("(* ?a (sum ?i ?b))").unwrap(),
+            PushMul {
+                a: "?a".parse().unwrap(),
+                i: "?i".parse().unwrap(),
+                b: "?b".parse().unwrap(),
+            }
+        ),
+        Rewrite::new(
+            "dim_subst",
+            Math::parse_pattern("(subst ?e (dim ?v ?m) (dim ?i ?n))").unwrap(),
+            DimSubst {
+                e: "?e".parse().unwrap(),
+                v: "?v".parse().unwrap(),
+                i: "?i".parse().unwrap(),
+                n: "?n".parse().unwrap(),
+            }
+        ),
+
+    ]
+}
+
+#[derive(Debug)]
+struct Foundit;
+
+#[derive(Debug)]
+struct SubstAgg {
+    e: QuestionMarkName,
+    v1: QuestionMarkName,
+    v2: QuestionMarkName,
+    body: QuestionMarkName,
+}
+
+#[derive(Debug)]
+struct SumIA {
+    i: QuestionMarkName,
+    a: QuestionMarkName,
+}
+
+#[derive(Debug)]
+struct PushMul {
+    i: QuestionMarkName,
+    a: QuestionMarkName,
+    b: QuestionMarkName,
+}
+
+#[derive(Debug)]
+struct PullMul {
+    i: QuestionMarkName,
+    a: QuestionMarkName,
+    b: QuestionMarkName,
+}
+
+#[derive(Debug)]
+struct DimSubst {
+    e: QuestionMarkName,
+    v: QuestionMarkName,
+    i: QuestionMarkName,
+    n: QuestionMarkName,
+}
+
+impl Applier<Math, Meta> for Foundit {
+    fn apply(&self, _egraph: &mut EGraph, _map: &WildMap) -> Vec<AddResult> {
+        panic!("FOUNDITTT")
+    }
+}
+
+impl Applier<Math, Meta> for SubstAgg {
+    fn apply(&self, egraph: &mut EGraph, map: &WildMap) -> Vec<AddResult> {
+        let v1 = map[&self.v1][0];
+        let v2 = map[&self.v2][0];
+        let e = map[&self.e][0];
+        let body = map[&self.body][0];
+
+        let res = if v1 == v2 {
+            egraph.add(Expr::new(Math::Agg, smallvec![v2, body]))
+        } else {
+            let sub_body = egraph.add(Expr::new(Math::Subst, smallvec![e, v1, body]));
+            egraph.add(Expr::new(Math::Agg, smallvec![v2, sub_body.id]))
+        };
+
+        vec![res]
+    }
+}
+
+impl Applier<Math, Meta> for SumIA {
+    fn apply(&self, egraph: &mut EGraph, map: &WildMap) -> Vec<AddResult> {
+        let i = map[&self.i][0];
+        let a = map[&self.a][0];
+
+        let i_m = egraph[i].metadata.clone();
+        let a_m = egraph[a].metadata.clone();
+
+        let mut res = Vec::new();
+
+        if let (Schema::Dims(k, n), Schema::Schema(body)) = (&i_m.schema, &a_m.schema) {
+            if !body.contains_key(k) {
+                let i_abs = egraph.add(Expr::new(Math::Num(*n as i32), smallvec![]));
+                let i_val = egraph.add(Expr::new(Math::Lit, smallvec![i_abs.id]));
+                let mul = egraph.add(Expr::new(Math::Mul, smallvec![a, i_val.id]));
+                res.push(mul);
+            }
+        } else {
+            panic!("wrong schema in aggregate i:{:?} body:{:?}", i_m.schema, a_m.schema);
+        }
+
+        res
+    }
+}
+
+
+impl Applier<Math, Meta> for PullMul {
+    fn apply(&self, egraph: &mut EGraph, map: &WildMap) -> Vec<AddResult> {
+        let i = map[&self.i][0];
+        let a = map[&self.a][0];
+        let b = map[&self.b][0];
+
+        let i_schema = egraph[i].metadata.clone();
+        let a_schema = egraph[a].metadata.clone();
+
+        let mut res = Vec::new();
+
+        if let (Schema::Dims(k, n), Schema::Schema(body)) = (&i_schema.schema, &a_schema.schema) {
+            if !body.contains_key(k) {
+                let agg = egraph.add(Expr::new(Math::Agg, smallvec![i, b]));
+                let mul = egraph.add(Expr::new(Math::Mul, smallvec![a, agg.id]));
+                res.push(mul);
+            }
+        } else {
+            panic!("wrong schema in aggregate i:{:?} body:{:?}", i_schema, a_schema);
+        }
+
+        res
+    }
+}
+
+impl Applier<Math, Meta> for PushMul {
+    fn apply(&self, egraph: &mut EGraph, map: &WildMap) -> Vec<AddResult> {
+        let a = map[&self.a][0];
+        let i = map[&self.i][0];
+        let b = map[&self.b][0];
+
+        let ((i_i, i_n), a_schema) = if let (Schema::Dims(i, n) , Schema::Schema(a_s))
+            = (egraph[i].metadata.clone().schema, egraph[a].metadata.clone().schema) {
+                ((i, n), a_s)
+            } else {
+                panic!("wrong schema in push multiply");
+            };
+
+        let mut res = Vec::new();
+
+        if !a_schema.contains_key(&i_i) {
+            let mul = egraph.add(Expr::new(Math::Mul, smallvec![a, b]));
+            let agg = egraph.add(Expr::new(Math::Agg, smallvec![i, mul.id]));
+            res.push(agg);
+        } else {
+            let mut s = DefaultHasher::new();
+            [i, a, b].hash(&mut s);
+            let fresh_s = "v".to_owned() + &(s.finish() % 976521).to_string();
+
+            let fresh_v = egraph.add(Expr::new(Math::Var(fresh_s), smallvec![]));
+            let fresh_n = egraph.add(Expr::new(Math::Num(i_n as i32), smallvec![]));
+            let fresh_dim = egraph.add(Expr::new(Math::Dim, smallvec![fresh_v.id, fresh_n.id]));
+
+            let b_subst = egraph.add(Expr::new(Math::Subst, smallvec![fresh_dim.id, i, b]));
+            let mul = egraph.add(Expr::new(Math::Mul, smallvec![a, b_subst.id]));
+            let agg = egraph.add(Expr::new(Math::Agg, smallvec![fresh_dim.id, mul.id]));
+            res.push(agg);
+        }
+        res
+    }
+}
+
+impl Applier<Math, Meta> for DimSubst {
+    fn apply(&self, egraph: &mut EGraph, map: &WildMap) -> Vec<AddResult> {
+        let e = map[&self.e][0];
+        let v = map[&self.v][0];
+        let i = map[&self.i][0];
+        let n = map[&self.n][0];
+
+        let res = if i == v {
+            AddResult {
+                was_there: true,
+                id: e
+            }
+        } else {
+            egraph.add(Expr::new(Math::Dim, smallvec![i, n]))
+        };
+
+        vec![res]
+    }
+}
+
