9270a2204a371377a6ecfefbcfe9f0e77e40d55e
remysucre <remy.sucre@gmail.com>
2020-01-29 13:06:40 -0800

    Fix compile time script v sml

--- compilevsml.py
+++ compilevsml.py
@@ -3,31 +3,26 @@ import numpy as np
 
 plt.rcParams.update({'font.size': 13})
 
+fig, ax = plt.subplots()
 width = 0.2
 
-fig, ax = plt.subplots()
+sysml  = [3, 2.8, 3, 3.28, 4.7328]
+greedy = [4, 4.8, 4.8, 4.8, 13]
+ilp    = [5, 6, 41, 21, 31]
 
-# k-means
 labels = ['GLM', 'L2SVM', 'MLogReg', 'ALS', 'PNMF']
-opt0 = [13.345, 22.8758, 114.7328, 114.7328, 114.7328]
-opt2_nofuse = [5, 6, 41, 41, 41]
-opt2_fuse = [4, 4.8, 4.8, 4.8, 33]
-
 x = np.arange(len(labels))
-ax.set_ylabel(ylabel='Compile Time [sec]', labelpad=-6)
 
-ax.bar(x-1.5*width, opt0,        width, label='SystemML', ec='black', color='0.2')
-ax.bar(x-0.5*width, opt2_nofuse, width, label='ILP', ec='black', color='0.5')
-ax.bar(x+0.5*width, opt2_fuse,   width, label='greedy', ec='black', color='white')
+ax.bar(x-1.5*width, sysml,  width, label='SystemML', ec='black', color='0.2')
+ax.bar(x-0.5*width, greedy, width, label='greedy',   ec='black', color='0.5')
+ax.bar(x+0.5*width, ilp,    width, label='ILP',      ec='black', color='white')
 
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
-ax.legend()
 ax.set_xlabel('Algorithm')
-
+ax.set_ylabel(ylabel='Compile Time [sec]')
 ax.label_outer()
-
-yl = plt.ylim()
+ax.legend()
 
 plt.tight_layout()
 plt.savefig('compilevsml.pdf', format='pdf')
